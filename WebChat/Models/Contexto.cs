8a2b2c4f357725cad8a78bbe45e15255db5f5f7c
Thiago Adalberto Paranhos Bucsan <thiago.bucsan@hotmail.com>
2020-07-02 00:51:04 -0300

    Add project files.

---
+++ WebChat/Models/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebChat.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<RespostaChat> RespostaChat { get; set; }
+
+
+    }
+}
---
+++ WebChat/Models/ResponseApi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebChat.Models
+{
+    public class ResponseApi
+    {
+        public string resposta { get; set; }
+    }
+}
